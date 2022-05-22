f041499ed00a97f44459ee5fe4200f4853b7d323
Michael Loncsek <michael.loncsek@tum.de>
2022-05-22 18:27:41 +0200

    coding challenge II done

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <espl_lib.h>
 
 int main()
 {
-    printf("Hello ESPL\n");
+    int i = 0;
+    char runagain;
+    bool again = true;
+
+    while(again)
+    {
+        printf("Enter a Number\n");
+        scanf("%i", &i);
+        printf("The number you entered was: %s\n", num_to_words(i));
+        printf("Do you want to run the program again= [y/n]\n");
+        scanf(" %c", &runagain);
+        if(runagain == 'n')
+        {
+            again = false;
+        }
+    }
+    return 0;
 }
